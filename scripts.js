d178bb9d7c480af44ffb75380770193dd64f155c
azvdu <gabriel.liradefarias2@gmail.com>
2022-04-10 17:52:02 -0300

    TERMINEI...... eu acho

--- scripts.js
+++ scripts.js
@@ -1,36 +1,41 @@
 let imagens = [
     "bobrossparrot.gif",
-    "bobrossparrot.gif",
-    "explodyparrot.gif",
     "explodyparrot.gif",
     "fiestaparrot.gif",
-    "fiestaparrot.gif",
-    "tripletsparrot.gif",
     "tripletsparrot.gif",
     "revertitparrot.gif",
-    "revertitparrot.gif",
     "metalparrot.gif",
-    "metalparrot.gif",
-    "unicornparrot.gif",
     "unicornparrot.gif"]
 
-// imagens.sort(embaralhar)
-// function embaralhar () {
-//     return Math.random() - 0.5;
-// }
-// let cartas = []
+imagens.sort(embaralhar)
+function embaralhar () {
+    return Math.random() - 0.5;
+}
+
+let cartas = []
+let numeroDeCartas = 0
 
 function colocarCartas () {
-    const numeroDeCartas = prompt("Com quantas cartas você quer jogar?")
+    numeroDeCartas = prompt("Com quantas cartas você quer jogar?")
     if (numeroDeCartas % 2 == 0 && numeroDeCartas >= 4 && numeroDeCartas <= 14) {
-        let cartas = document.querySelector(".jogo")
-        for (let i = 0; i < numeroDeCartas; i++){
-            cartas.innerHTML += `
+        let jogo = document.querySelector(".jogo")
+        for (let i = 0; i < (numeroDeCartas/2); i++){
+            cartas.push( `
             <div class="carta" onclick="virarCarta(this)">
                 <img src="Imagens/front.png">
                 <img class="escondido" src="Imagens/${imagens[i]}">
-            </div>
-            `
+            </div>`)
+            cartas.push(`
+            <div class="carta" onclick="virarCarta(this)">
+                <img src="Imagens/front.png">
+                <img class="escondido" src="Imagens/${imagens[i]}">
+            </div>`)
+            cartas.sort(embaralhar) 
+        }
+        cartas.sort(embaralhar)
+
+        for (let i = 0; i < cartas.length; i++) {
+            jogo.innerHTML += cartas[i]
         }
 
     } else {
@@ -38,30 +43,54 @@ function colocarCartas () {
     }
 } colocarCartas()
 
-let par = []
+let frentes = []
+let versos = []
+let jogadas = 0
+let numeroDeAcertos = 0
 
 function virarCarta (carta) {
+    jogadas += 1
+    console.log(jogadas)
     let frente = carta.children[0]
     frente.classList.add("escondido")
     let verso = carta.children[1]
     verso.classList.remove("escondido")
-    par.push(verso.src)
+    versos.push(verso)
+    frentes.push(frente)
+    carta.removeAttribute("onclick")
 
-    console.log(par);
-    if (par[0] == par[1] && par.length == 2 ){
-        console.log("acertou papai")
-        par = []
-    }
-    else if (par.length == 2) {
-        console.log("errou meu fi")
-        par = []
-        desvirarCarta()
+    if(versos.length == 2) {
+        if (versos[0].src == versos[1].src){
+            versos = []
+            frentes = []
+            numeroDeAcertos += 1
+            console.log("acertou papai", numeroDeAcertos)
+            setTimeout(verificarVitoria, 100)
+        }
+        else {
+            console.log("errou meu fi")
+            setTimeout(desvirarCarta, 1000)
+            
+            let carta1 = frentes[0].parentNode;
+            carta1.setAttribute("onclick", "virarCarta(this)")
+            let carta2 = frentes[1].parentNode;
+            carta2.setAttribute("onclick", "virarCarta(this)")
+
+        }
+        
     }
 }
 function desvirarCarta () {
-    let frente = carta.children[0]
-    frente.classList.remove("escondido")
-    let verso = carta.children[1]
-    verso.classList.add("escondido")
-    setTimeout(desvirarCarta, 1000)
+    versos[0].classList.add("escondido")
+    versos[1].classList.add("escondido")
+    frentes[0].classList.remove("escondido")
+    frentes[1].classList.remove("escondido")
+    versos = []
+    frentes = []
+}
+function verificarVitoria () {
+    console.log(numeroDeAcertos, numeroDeCartas)
+    if (numeroDeAcertos == (numeroDeCartas/2)) {
+        alert (`Você ganhou em ${jogadas} jogadas!`)
+    }
 }
